10c95d7c3b5818767f72442447129f1ea041bad0
Mihai Scurtu <mscurtu90@gmail.com>
2013-09-28 17:48:05 +0300

    Base work for AI

---
+++ Roguelike/src/entity/Actor.java
@@ -0,0 +1,5 @@
+package entity;
+
+public interface Actor {
+	public void takeTurn();
+}
--- Roguelike/src/entity/Creature.java
+++ Roguelike/src/entity/Creature.java
@@ -1,10 +1,13 @@
 package entity;
 
-public class Creature extends Entity {
+public abstract class Creature extends Entity implements Actor {
 
 	public Creature(String type, String name, int x, int y) {
 		super(type, name, x, y);
 		// TODO Auto-generated constructor stub
 	}
+
+	@Override
+	public abstract void takeTurn();
 
 }
